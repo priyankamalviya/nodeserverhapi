4ae79f4b0a2a21e399bc53732facb06b52277c55
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-22 15:27:28 -0800

    start node server at port 8000

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 'use strict'
 const Hapi = require('hapi')
+
+const server = new Hapi.Server()
+
+server.connection({
+  host: 'localhost',
+  port: 8000
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    reply('Hello there from Hapi!!')
+  }
+})
+
+server.start(() =>
+  console.log(`Started at: ${server.info.uri}`)
+)
